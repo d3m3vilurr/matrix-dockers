8580c7b41bf453f19b279d0bbfdc0cb228082d71
Sunguk Lee <d3m3vilurr@gmail.com>
2018-10-12 15:37:13 +0900

    Initial commit

---
+++ matrix-puppet-facebook/build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+docker build -t matrix-puppet-facebook .
+curl -L https://github.com/matrix-hacks/matrix-puppet-facebook/raw/master/config.sample.json -o config.sample.json
+docker image prune -f
