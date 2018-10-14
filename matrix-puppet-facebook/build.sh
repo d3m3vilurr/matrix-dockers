782163dae616ff5e3254b149d0369ccfb9ea9e2d
Sunguk Lee <d3m3vilurr@gmail.com>
2018-10-14 20:25:04 +0900

    Remove prune part
    occur hanging after build

--- matrix-puppet-facebook/build.sh
+++ matrix-puppet-facebook/build.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 docker build -t matrix-puppet-facebook .
 curl -L https://github.com/matrix-hacks/matrix-puppet-facebook/raw/master/config.sample.json -o config.sample.json
-docker image prune -f
